4e6bd8ac11a70c4c1b07f70dbb8f779d74874294
jinju0228 <jin_ju0228@daum.net>
2022-07-05 15:33:33 +0900

    logout add

--- public/js/nmarket.js
+++ public/js/nmarket.js
@@ -1,6 +1,8 @@
 Kakao.init('9e690ea3b0d9e5d9bf13d2f47c70063a');
 Kakao.isInitialized();
 
+
+document.getElementById('logout').style.display = "none"
 function kakaoLogin(){
     Kakao.Auth.login({
         success: function (response) {
@@ -8,7 +10,7 @@ function kakaoLogin(){
                 url:'/v2/user/me',
                 success: function (response) { 
                     console.log(response);
-                    document.getElementById('user').innerText = response.kakao_account.profile.nickname;
+                    document.getElementById('user').innerText = response.kakao_account.profile.nickname + '님';
                     document.getElementById('login').style.display = "none";
                     alert(response.kakao_account.profile.nickname + '님 로그인 되었습니다.')
                 }
@@ -20,5 +22,21 @@ function kakaoLogin(){
 
 
 function kakaoLogout(){
+    if (!Kakao.Auth.getAccessToken()) {
+        Kakao.API.request({
+            url:'/v1/user/unlink',
+            success: function (response) { 
+                console.log(response);
+                document.getElementById('user').style.display = "none";
+                document.getElementById('login').style.display = "block";
+                document.getElementById('loout').style.display = "none";
+                alert('로그아웃 되었습니다.')
+            }
+        })
 
-}
+        Kakao.Auth.setAccessTocken(undefined)
+      }
+    //   Kakao.Auth.logout(function() {
+    //     alert('logout ok\naccess token -> ' + Kakao.Auth.getAccessToken())
+    //   })
+    }
